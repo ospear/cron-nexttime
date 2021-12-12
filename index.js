32d3b76694cef7bbb18826e8195cdf766cec92a0
Yoshimitsu Ooe <ooe@ospear.com>
2021-12-12 23:46:53 +0900

    Support other wildcard

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+function matchRange(part, value) {
+  const [start, end] = part.split('-')
+  return start <= value && value <= end
+}
+
+function matchIncrement(part, value) {
+  const [offset, per] = part.split('/')
+  return value % per == (offset === '*' ? 0 : offset)
+}
+
 class MinuteField {
   constructor(field) {
     this.field = field
   }
+
   match(time) {
-    return this.field === '*' || this.field == time.getMinutes()
+    const value = time.getMinutes()
+    const split = this.field.split(',')
+    for (const f of split) {
+      if (f === '*') {
+        return true
+      }
+      if (f.includes('-')) {
+        return matchRange(f, value)
+      }
+      if (f.includes('/')) {
+        return matchIncrement(f, value)
+      }
+      if (f == value) {
+        return true
+      }
+    }
+    return false
   }
 }
 
@@ -11,8 +38,25 @@ class HourField {
   constructor(field) {
     this.field = field
   }
+
   match(time) {
-    return this.field === '*' || this.field == time.getHours()
+    const value = time.getHours()
+    const split = this.field.split(',')
+    for (const f of split) {
+      if (f === '*') {
+        return true
+      }
+      if (f.includes('-')) {
+        return matchRange(f, value)
+      }
+      if (f.includes('/')) {
+        return matchIncrement(f, value)
+      }
+      if (f == value) {
+        return true
+      }
+    }
+    return false
   }
 }
 
@@ -20,8 +64,37 @@ class DateField {
   constructor(field) {
     this.field = field
   }
+
   match(time) {
-    return this.field === '*' || this.field == time.getDate()
+    const value = time.getDate()
+    const split = this.field.split(',')
+    for (const f of split) {
+      if (f === '*' || f === '?') {
+        return true
+      }
+      if (f === 'L') {
+        const tomorrow = new Date(time.getTime() + 24 * 60 * 60 * 1000)
+        return time.getMonth() != tomorrow.getMonth()
+      }
+      if (f.includes('W')) {
+        const [nth] = f.split('W')
+        const dow = time.getDay() + 1
+        if (nth === '') {
+          return matchRange('2-6', dow)
+        }
+        return time.getDate() >= Number(nth) && matchRange('2-6', dow)
+      }
+      if (f.includes('-')) {
+        return matchRange(f, value)
+      }
+      if (f.includes('/')) {
+        return matchIncrement(f, value)
+      }
+      if (f == value) {
+        return true
+      }
+    }
+    return false
   }
 }
 
@@ -29,8 +102,25 @@ class MonthField {
   constructor(field) {
     this.field = field
   }
+
   match(time) {
-    return this.field === '*' || this.field == time.getMonth() + 1
+    const value = time.getMonth() + 1
+    const split = this.field.split(',')
+    for (const f of split) {
+      if (f === '*') {
+        return true
+      }
+      if (f.includes('-')) {
+        return matchRange(f, value)
+      }
+      if (f.includes('/')) {
+        return matchIncrement(f, value)
+      }
+      if (f == value) {
+        return true
+      }
+    }
+    return false
   }
 }
 
@@ -38,8 +128,32 @@ class DayOfWeekField {
   constructor(field) {
     this.field = field
   }
+
   match(time) {
-    return this.field === '*' || this.field == time.getDay() + 1
+    const value = time.getDay() + 1
+    const split = this.field.split(',')
+    for (const f of split) {
+      if (f === '*' || f === '?') {
+        return true
+      }
+      if (f === 'L') {
+        // TODO: sunday?
+        return value === 1
+      }
+      if (f.includes('-')) {
+        return matchRange(f, value)
+      }
+      if (f.contains('#')) {
+        const [dow, nth] = f.split('#')
+        return (
+          dow === value && Math.floor(Number(time.getDate()) / 7) + 1 === nth
+        )
+      }
+      if (f == value) {
+        return true
+      }
+    }
+    return false
   }
 }
 
@@ -56,6 +170,7 @@ class CronNextTime {
     this.now = new Date(currentTime.getTime())
     this.now.setSeconds(0)
     this.now.setMilliseconds(0)
+    this.nextTime = new Date(this.now.getTime() + 60 * 1000)
   }
 
   /**
@@ -108,41 +223,75 @@ class CronNextTime {
     )
   }
 
-  nextTick(time) {
-    if (!this.month.match(time)) {
-      time.setMonth(time.getMonth() + 1)
-      time.setDate(1)
-      time.setHours(0)
-      time.setMinutes(0)
-      return time
-    }
-    if (!this.date.match(time) || !this.dayOfWeek.match(time)) {
-      time.setDate(time.getDate() + 1)
-      time.setHours(0)
-      time.setMinutes(0)
-      return time
-    }
-    if (!this.hour.match(time)) {
-      time.setHours(time.getHours() + 1)
-      time.setMinutes(0)
-      return time
-    }
-    time.setMinutes(time.getMinutes() + 1)
-    return time
-  }
-
   next() {
-    let time = new Date(this.now.getTime())
     do {
-      time = this.nextTick(time)
-      // console.log({ time, now: this.now, fields: this.fields })
-      if (time - this.now > 366 * 24 * 60 * 60 * 1000) {
+      this.nextMonth()
+      this.nextDate()
+      this.nextHour()
+      this.nextMinute()
+
+      if (this.nextTime - this.now > (1 + 4 * 365) * 24 * 60 * 60 * 1000) {
         throw new Error(
-          `Not found next time. Searched until ${time}: now=${this.now}`,
+          `Not found next time. Searched until ${this.nextTime}: now=${this.now}`,
         )
       }
-    } while (!this.match(time))
-    return time
+    } while (!(this.match(this.nextTime) && this.now < this.nextTime))
+
+    return this.nextTime
+  }
+
+  nextMinute() {
+    for (let i = 0; i < 60; i++) {
+      if (this.minute.match(this.nextTime)) {
+        return
+      }
+      this.nextTime.setMinutes(this.nextTime.getMinutes() + 1)
+    }
+    throw new Error(
+      `Not found next time. Searched until ${this.nextTime}: now=${this.now}`,
+    )
+  }
+
+  nextHour() {
+    for (let i = 0; i < 24; i++) {
+      if (this.hour.match(this.nextTime)) {
+        return
+      }
+      this.nextTime.setHours(this.nextTime.getHours() + 1)
+      this.nextTime.setMinutes(0)
+    }
+    throw new Error(
+      `Not found next time. Searched until ${this.nextTime}: now=${this.now}`,
+    )
+  }
+
+  nextDate() {
+    for (let i = 0; i < 31; i++) {
+      if (
+        this.date.match(this.nextTime) &&
+        this.dayOfWeek.match(this.nextTime)
+      ) {
+        return
+      }
+      this.nextTime.setDate(this.nextTime.getDate() + 1)
+      this.nextTime.setHours(0)
+      this.nextTime.setMinutes(0)
+    }
+  }
+
+  nextMonth() {
+    for (let i = 0; i < 12; i++) {
+      if (this.month.match(this.nextTime)) {
+        return
+      }
+      this.nextTime.setMonth(this.nextTime.getMonth() + 1)
+      this.nextTime.setDate(1)
+      this.nextTime.setHours(0)
+      this.nextTime.setMinutes(0)
+    }
+    throw new Error(
+      `Not found next time. Searched until ${this.nextTime}: now=${this.now}`,
+    )
   }
 }
 
